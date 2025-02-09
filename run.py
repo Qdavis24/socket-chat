96b1ea17eef7a9a05458a336b0a6298ee65f3340
Qdavis24 <quoncdavis@gmail.com>
2025-02-08 17:02:01 -0700

    add prod config

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app
+app, socketio = create_app()
 
 if __name__ == "__main__":
-    app, socketio = create_app()
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app)
